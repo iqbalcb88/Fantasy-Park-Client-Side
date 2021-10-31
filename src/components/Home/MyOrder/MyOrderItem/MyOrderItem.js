1b24c64933fd2ad889189c1f7ebaab38e3bf9623
iqbalcb88 <iqbal.cb88@gmail.com>
2021-10-31 19:36:48 +0600

    make my order page responsive for xs device

--- src/components/Home/MyOrder/MyOrderItem/MyOrderItem.js
+++ src/components/Home/MyOrder/MyOrderItem/MyOrderItem.js
@@ -5,19 +5,19 @@ const MyOrderItem = ({ item, handleDelete }) => {
   const { _id, name, price, quantity, status } = item;
   return (
     <Row className=' py-3'>
-      <Col>
+      <Col xs={3}>
         <h4>OrderName: {name}</h4>
       </Col>
-      <Col>
+      <Col xs={2}>
         <p>Price: {price}</p>
       </Col>
-      <Col>
+      <Col xs={2}>
         <p>Quantity: {quantity}</p>
       </Col>
-      <Col>
+      <Col xs={2}>
         <button className='btn btn-info'>{status}</button>
       </Col>
-      <Col>
+      <Col xs={3}>
         <button
           onClick={() => {
             handleDelete(_id);
